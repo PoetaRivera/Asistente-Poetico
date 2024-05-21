778a7d8fc9d8a50cecaa7e675e5b2cff9ff79146
NELSON RIVERA <nrrvrivera@hotmail.com>
2024-05-21 07:43:48 -0600

    cambio de presentacion1

---
+++ funciones.js
@@ -0,0 +1,23 @@
+
+/*export function limpiar() {
+  // limpia todo
+  intext1.val("");
+  outtext1.val("");
+  outtext2.val("");
+  outtext3.val("");
+  outtext4.val("");
+} */
+
+export function prueba() {
+    let x = "holaaaaaaaaaaaaaaa";
+    console.log(x);
+}
+
+//Hacer alto de pantalla igual a la pantalla del dispositivo.
+/*export function obtenerTamanoPantalla() {
+  let ancho = screen.width;
+  let alto = screen.height;
+  alto = 1 * alto;
+  ancho = 1 * ancho;
+  $("body").css("height", alto + "px");
+}*/
